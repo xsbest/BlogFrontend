da23394780f31d5efd9adb9b7e1c1e1a7bc56dc6
unknown <https://github.com/Danneil-525/my-notes.git>
2021-08-04 13:09:38 +0800

    feat: 封装了List组件和Footer组件

---
+++ components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.less'
+
+export default function Footer() {
+  return (
+    <div className={styles.container}>
+      <div>The system is made by React+Node+Antd</div>
+      <div>@copyright Daniel</div>
+    </div>
+  )
+}
